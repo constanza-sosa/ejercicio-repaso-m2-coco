6827f6d59d66c7033967b417bc773c88923c4712
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-07-25 21:22:43 +0200

    Better format

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-const button = document.querySelector('.btn');
-const title = document.querySelector('.title');
-const selection = document.querySelector('.select1');
+const button = document.querySelector(".btn");
+const title = document.querySelector(".title");
+const selection = document.querySelector(".select1");
 
-const comoEstoy = function (){
+const comoEstoy = function() {
   const mood = selection.value;
   console.log(`Estoy ${mood}`);
-}
+};
 
-function yourMood(){
+function yourMood() {
   // me gustaría utilizar la constante anterior comoEstoy aquí en vez de selection.value
-  if(selection.value === 'happy'){
-    title.innerHTML = ':)';
-  }
-  else{
-    title.innerHTML = ':('
+  if (selection.value === "happy") {
+    title.innerHTML = ":)";
+  } else {
+    title.innerHTML = ":(";
   }
 }
 
-selection.addEventListener('change', comoEstoy);
-button.addEventListener('click', yourMood);
+selection.addEventListener("change", comoEstoy);
+button.addEventListener("click", yourMood);
