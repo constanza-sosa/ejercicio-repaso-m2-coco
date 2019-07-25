fcca55db332042c7441759ba0b4852cad4329a87
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-07-25 21:48:02 +0200

    Asked commit

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ const selection = document.querySelector(".select1");
 const comoEstoy = function() {
   const mood = selection.value;
   console.log(`Estoy ${mood}`);
+  return mood
 };
 
 function yourMood() {
   // me gustaría utilizar la constante anterior comoEstoy aquí en vez de selection.value
-  if (selection.value === "happy") {
+  if (comoEstoy === "happy") {
     title.innerHTML = ":)";
   } else {
     title.innerHTML = ":(";
